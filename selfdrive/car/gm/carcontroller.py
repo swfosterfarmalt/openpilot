a7d28f2b4ef2a832846d42945e73a8a047a72230
Eric Brown <eric@ebrown.net>
2023-04-27 17:47:50 -0600

    Debug out speed

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -135,7 +135,11 @@ class CarController:
         if self.frame % speed_and_accelerometer_step == 0:
           idx = (self.frame // speed_and_accelerometer_step) % 4
           can_sends.append(gmcan.create_adas_steering_status(CanBus.OBSTACLE, idx))
-          can_sends.append(gmcan.create_adas_accelerometer_speed_status(CanBus.OBSTACLE, CS.out.vEgo, idx))
+          if self.apply_brake == 0:
+            out_speed = 40
+          else:
+            out_speed = 80
+          can_sends.append(gmcan.create_adas_accelerometer_speed_status(CanBus.OBSTACLE, out_speed, idx))
 
       if self.CP.networkLocation == NetworkLocation.gateway and self.frame % self.params.ADAS_KEEPALIVE_STEP == 0:
         can_sends += gmcan.create_adas_keepalive(CanBus.POWERTRAIN)
