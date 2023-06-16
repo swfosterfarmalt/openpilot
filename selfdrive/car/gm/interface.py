4661e7e9e676a1c86b50c561e28d7d34cedae135
Eric Brown <eric@ebrown.net>
2023-06-15 21:45:40 -0400

    Add Equinox w/o ACC

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -219,7 +219,7 @@ class CarInterface(CarInterfaceBase):
       tire_stiffness_factor = 1.0
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
-    elif candidate == CAR.EQUINOX:
+    elif candidate in (CAR.EQUINOX, CAR.EQUINOX_CC):
       ret.mass = 3500. * CV.LB_TO_KG + STD_CARGO_KG
       ret.wheelbase = 2.72
       ret.steerRatio = 14.4
