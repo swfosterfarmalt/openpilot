74d479592f226ae7022f2ded21a0c176e14786cb
Eric Brown <eric@ebrown.net>
2023-06-17 17:52:14 -0500

    Add CC long tx message list

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -134,7 +134,7 @@ class CarController:
 
         at_full_stop = CC.longActive and CS.out.standstill
         if CC.longActive and self.CP.carFingerprint in CC_ONLY_CAR and not CS.CP.enableGasInterceptor:
-          # Using extend instead of append since the message is only sent on speed change
+          # Using extend instead of append since the message is only sent intermittently
           can_sends.extend(gmcan.create_gm_cc_spam_command(self.packer_pt, self, CS, actuators))
         elif CS.CP.enableGasInterceptor:
           can_sends.append(gmcan.create_gm_pedal_interceptor_command(self.packer_pt, CS, CC, actuators, idx))
