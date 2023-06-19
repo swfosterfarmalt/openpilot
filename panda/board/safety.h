80e2cfdb6b331c7e0d7f3269463c03b356455025
Eric Brown <eric@ebrown.net>
2023-06-19 17:33:13 -0500

    Disable relay malfunction check

--- panda/board/safety.h
+++ panda/board/safety.h
@@ -272,7 +272,8 @@ void generic_rx_checks(bool stock_ecu_detected) {
 
   // check if stock ECU is on bus broken by car harness
   if ((safety_mode_cnt > RELAY_TRNS_TIMEOUT) && stock_ecu_detected) {
-    relay_malfunction_set();
+    (void) 0;
+//    relay_malfunction_set();
   }
 }
 
