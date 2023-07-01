f76720647552ded67d7eb97653917317fbbbf1fd
Eric Brown <eric@ebrown.net>
2023-06-30 23:21:17 -0600

    Use generic interceptor command function, always send ACC

--- selfdrive/car/gm/carstate.py
+++ selfdrive/car/gm/carstate.py
@@ -280,19 +280,3 @@ class CarState(CarStateBase):
     ]
 
     return CANParser(DBC[CP.carFingerprint]["pt"], signals, checks, CanBus.LOOPBACK, enforce_checks=False)
-
-  @staticmethod
-  def actuator_hystereses(final_pedal, pedal_steady):
-    # hyst params... TODO: move these to VehicleParams
-    pedal_hyst_gap = 0.01  # don't change pedal command for small oscillations within this value
-
-    # for small pedal oscillations within pedal_hyst_gap, don't change the pedal command
-    if math.isclose(final_pedal, 0.0):
-      pedal_steady = 0.
-    elif final_pedal > pedal_steady + pedal_hyst_gap:
-      pedal_steady = final_pedal - pedal_hyst_gap
-    elif final_pedal < pedal_steady - pedal_hyst_gap:
-      pedal_steady = final_pedal + pedal_hyst_gap
-    final_pedal = pedal_steady
-
-    return final_pedal, pedal_steady
