cd5e1899a2c6660ab5203163ad8d95a84e03fc2b
Eric Brown <eric@ebrown.net>
2023-07-03 08:30:23 -0600

    Send next index for button spamming

--- selfdrive/car/gm/gmcan.py
+++ selfdrive/car/gm/gmcan.py
@@ -240,6 +240,7 @@ def create_gm_cc_spam_command(packer, controller, CS, actuators):
   # TODO: Cleanup the timing - normal is every 30ms...
   if (cruiseBtn != CruiseButtons.INIT) and ((controller.frame - controller.last_button_frame) * DT_CTRL > rate):
     controller.last_button_frame = controller.frame
-    return [create_buttons(packer, CanBus.POWERTRAIN, CS.buttons_counter, cruiseBtn)]
+    idx = (CS.buttons_counter + 1) % 4  # Need to predict the next idx for '22-23 EUV
+    return [create_buttons(packer, CanBus.POWERTRAIN, idx, cruiseBtn)]
   else:
     return []
