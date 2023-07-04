6e13fa2bd7b6048f220168d738dc035839bb1633
Eric Brown <eric@ebrown.net>
2023-07-04 13:41:33 -0600

    Fix CS cruise state check

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -176,7 +176,7 @@ class CarController:
         can_sends += gmcan.create_adas_keepalive(CanBus.POWERTRAIN)
 
       # TODO: integrate this with the code block below?
-      if self.CP.enableGasInterceptor and self.CP.carFingerprint in CC_ONLY_CAR and CS.cruiseState.enabled:
+      if self.CP.enableGasInterceptor and self.CP.carFingerprint in CC_ONLY_CAR and CS.out.cruiseState.enabled:
         if (self.frame - self.last_button_frame) * DT_CTRL > 0.04:
           self.last_button_frame = self.frame
           can_sends.append(gmcan.create_buttons(self.packer_pt, CanBus.POWERTRAIN, (CS.buttons_counter + 1) % 4, CruiseButtons.CANCEL))
